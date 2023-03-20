cfc206164d10ed1635760e44c561869865ee979b
Albert <albertpeiret@gmail.com>
2023-03-20 14:23:36 +0100

    Modify Tutorial example

--- examples/Tutorial.m
+++ examples/Tutorial.m
@@ -1,7 +1,7 @@
 clearvars
-close all % windows
+close all
 
-% Create Pendulum model
+%% Create Pendulum model
 
 % Model Parameters
 length = 1;
@@ -11,49 +11,51 @@ mass = 1;
 model = Model();
 model.name = "pendulum";
 
-% First link of the pendulum
+%% Create pendulum body
 body = Body();
-body.name                  	= "pendulum";
-body.mass                  	= 1;
-body.com                   	= [length/2; 0]; % center of mass
-body.inertia              	= mass * length / 12;
-body.geometry.points(:,1)  	= [0; 0];
-body.geometry.points(:,2)  	= [length; 0];
-body.geometry.lineColor    	= 'r';
-body.geometry.lineWidth    	= 4;
+body.name               = "pendulum";
+body.mass               = 1;
+body.com                = [length/2; 0]; % center of mass
+body.inertia            = mass * length / 12;
+
+% Define Body geometry
+body.geometry.lineColor = 'r';
+body.geometry.lineWidth = 4;
+body.geometry.addPoint([0; 0]);
+body.geometry.addPoint([length; 0]);
 
 % Add body to the model
 model.addBody(body);
 
-% Joint between the first link and the ground
+%% Create revolute Joint with the ground
 joint = JointRevolute();
-joint.parent              	= model.ground;
-joint.child             	= body;
-joint.pointParent         	= [0; 0];
-joint.pointChild           	= body.geometry.points(:,1);
+joint.parent        = model.ground;
+joint.child         = body;
+joint.pointParent   = [0; 0];
+joint.pointChild    = body.geometry.points(:,1);
 
 % Add joint to model
 model.addJoint(joint);
 
-% Angle between the first link and the ground
+%% Angle between pendulum and the ground
 coord = Coordinate();
-coord.type               	= "angular";
-coord.body                	= body;
-coord.initValue             = 0;
-coord.initSpeed             = 0;
+coord.type          = "angular";
+coord.body          = body;
+coord.initValue     = 0;
+coord.initSpeed     = 0;
 
 % Add coordinate to model
 model.addCoordinate(coord);
 
-% Initialize model and viewer
+%% Initialize model and viewer
 model.initModel();
-model.initViewer([-1, +1], [-1, +1]);
-model.viewer.showAxes(false);
+model.initViewer();
 
-%% Setup simulation parameters
+%% Simulate pendulum
+
+% Setup simulation parameters
 sim = Simulation(model);
-sim.integrator  = "EulerImplicit";
-sim.timeEnd     = 2;
+sim.timeEnd     = 5;
 sim.timeStep    = 0.01;
 
 % Initialize and run simulation
